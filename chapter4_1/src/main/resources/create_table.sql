053686cb9a651b5f3efc19adcb5d74d40a1a9331
liuwenke <qwfys200@qq.com>
2020-04-15 01:08:40 +0800

    update

--- chapter4_1/src/main/resources/create_table.sql
+++ chapter4_1/src/main/resources/create_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `springDemo`.`users` (
+CREATE TABLE `users` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
   `password` varchar(60) NOT NULL,
@@ -6,7 +6,7 @@ CREATE TABLE `springDemo`.`users` (
   `roles` text CHARACTER SET utf8 COMMENT '用户角色，多个角色之间用逗号隔开',
   PRIMARY KEY (`id`),
   KEY `username` (`username`)
-)
+);
 
-insert into `springDemo`.`users`(`username`, `password`, `roles`) values("admin", "123", "ROLE_ADMIN,ROLE_USER");
-insert into `springDemo`.`users`(`username`, `password`, `roles`) values("user", "123", "ROLE_USER");
+insert into `users`(`username`, `password`, `roles`) values("admin", "123456", "ROLE_ADMIN,ROLE_USER");
+insert into `users`(`username`, `password`, `roles`) values("user", "123456", "ROLE_USER");
